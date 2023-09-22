21e6d0a2458b91829c91c1f9995968660e41b2a0
jaylen <jaylen.work@hotmail.com>
2023-09-22 22:32:56 +0800

    feat: 初始化模板

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
