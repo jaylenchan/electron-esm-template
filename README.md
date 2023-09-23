e03cee0296fd4ea36e06b2b1d10ca945b17665f4
jaylen <jaylen.work@hotmail.com>
2023-09-23 11:12:50 +0800

    refactor: 修改代码

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## 相关链接
 
 - <https://github.com/electron/electron/issues/21457>
+
+## issue
+
+- electron-builder 设置成`asar=true`打包后无法启动electron
--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath, format } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    });
+    try {
+        const win = new BrowserWindow({
+            width: 800,
+            height: 600
+        });
 
-    win.loadURL(
-        url.format({
-            pathname: path.join(__dirname, 'index.html'),
-            protocol: 'file:',
-            slashes: true
-        })
-    );
+        win.loadURL(
+            format({
+                pathname: path.join(__dirname, 'index.html'),
+                protocol: 'file:',
+                slashes: true
+            })
+        );
+    } catch (err) {
+        throw err;
+    }
 };
 
 app.whenReady().then(() => {
